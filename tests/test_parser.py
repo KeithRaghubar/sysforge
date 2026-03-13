50195ec72e16623419d0d5e19b578c79f04ece72
Keith Raghubar <KeithRaghubar@users.noreply.github.com>
2026-03-12 20:11:39 -0400

    fix hardcoded paths in test_parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import os
 import pprint
 import sys
+from pathlib import Path
+
 from sysforge.primitives.pkgbuild_meta import parse_pkgbuild
 
-sys.path.insert(0, os.path.expanduser("~/src/sysforge"))
-
-PKGBUILD = f"{sys.path[0]}/tests/data/PKGBUILDs/complex2.PKGBUILD"
+TESTS_DIR = Path(__file__).parent
+PKGBUILD = TESTS_DIR / "data/PKGBUILDs/complex2.PKGBUILD"
 
 result = parse_pkgbuild(PKGBUILD)
 pprint.pprint(result)
